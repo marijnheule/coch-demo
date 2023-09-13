d94b77964c73d843a59052bd445c3720c20cd912
Marijn Heule <mheule@Marijns-Air.lan>
2023-09-13 19:00:07 -0400

    fix

--- run-output.sh
+++ run-output.sh
@@ -2,7 +2,7 @@ CNF=$1
 
 rm -f pipe$$ output$$
 mkfifo pipe$$
-./cake_lpr/cake_lpr $CNF pipe$$ > output &
+./cake_lpr/cake_lpr $CNF pipe$$ > output$$ &
 ./cadical/build/cadical $CNF pipe$$ --lrat --no-binary --sat
 wait
 cat output$$
