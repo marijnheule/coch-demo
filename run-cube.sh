f519a7d5ab5b760f138d4e4370d8e5acd8fc4770
Marijn Heule <mheule@Marijns-Air.lan>
2023-09-18 18:03:09 -0400

    ins

--- run-cube.sh
+++ run-cube.sh
@@ -1,4 +1,5 @@
 CNF=cube$$.cnf
+INS=$1
 DIR=$2
 
 if [ -z "$DIR" ]
@@ -7,10 +8,10 @@ then
 fi
 
 ./build/6gon-final > $DIR/tmp$$.cnf
-./build/gen-6gon-cube $1 > $DIR/tmp$$.cube
+./build/gen-6gon-cube $INS > $DIR/tmp$$.cube
 ./build/apply.sh $DIR/tmp$$.cnf $DIR/tmp$$.cube 1 > $CNF
 rm $DIR/tmp$$.cnf $DIR/tmp$$.cube
-md5sum $DIR/$CNF | cut -c1-10 | awk '{print "MD5: "$1"\n"}'
+md5sum $DIR/$CNF | cut -c1-10 | awk '{print "MD5: "$INS"\n"}'
 
 mkfifo pipe$$
 ./cake_lpr/cake_lpr $DIR/$CNF pipe$$ &
