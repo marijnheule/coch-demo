06578c2763faa34e0e5f7d57549ff865cdc12264
Marijn Heule <mheule@Marijns-Air.lan>
2023-09-18 18:07:30 -0400

    MD5

--- run-cube.sh
+++ run-cube.sh
@@ -11,7 +11,7 @@ fi
 ./build/gen-6gon-cube $INS > $DIR/tmp$$.cube
 ./build/apply.sh $DIR/tmp$$.cnf $DIR/tmp$$.cube 1 > $CNF
 rm $DIR/tmp$$.cnf $DIR/tmp$$.cube
-md5sum $DIR/$CNF | cut -c1-10 | awk '{print "MD5: "$INS"\n"}'
+md5sum $DIR/$CNF | cut -c1-10 | awk '{print "MD5: "$1"\n"}'
 
 mkfifo pipe$$
 ./cake_lpr/cake_lpr $DIR/$CNF pipe$$ &
